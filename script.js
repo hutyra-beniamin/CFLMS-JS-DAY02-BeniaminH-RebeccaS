12506916e1a99b728c4820c305a48bac646d16b7
Rebecca Schedler <rebecca.schedler@gmx.net>
2020-05-26 13:29:20 +0200

    intermediat exercise

--- script.js
+++ script.js
@@ -40,3 +40,30 @@ var Thomas = 85;
 var Klaus = 65;
 var Maria = 93;
 var David = 81;
+
+document.write("The average grade of the class is " + '' + (Martin + Thomas + Klaus + Maria + David) / 5)
+
+
+
+var student_grades = [
+["Martin", 76],
+["Thomas", 85],
+["Klaus", 65],
+["Maria", 93],
+["David", 81]
+];
+
+console.log(student_grades[1][1]);
+for (var i = 0; i < student_grades.length; i++) {
+    if (student_grades[i][1]<60) {
+        document.write(student_grades[i][0] + "\’s Grade with " + student_grades[i][1] + " Points is: F<br>");
+    } else if (student_grades[i][1]<70) {
+        document.write(student_grades[i][0] + "\’s Grade with " + student_grades[i][1] + " Points is: D<br>");
+    } else if (student_grades[i][1]<80) {
+        document.write(student_grades[i][0] + "\’s Grade with " + student_grades[i][1] + " Points is: C<br>");
+    } else if (student_grades[i][1]<90) {
+        document.write(student_grades[i][0] + "\’s Grade with " + student_grades[i][1] + " Points is: B<br>");
+    } else {
+        document.write(student_grades[i][0] + "\’s Grade with " + student_grades[i][1] + " Points is: A<br>");
+}
+}
