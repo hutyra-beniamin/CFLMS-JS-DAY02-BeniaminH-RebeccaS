7552e20c3e9ab18776df415da0b51487f4e9f49e
Beniamin Hutyra <hutyra.beniamin@outlook.com>
2020-05-26 11:16:31 +0200

    changes and content

--- script.js
+++ script.js
@@ -11,6 +11,22 @@ if (randomTemp > 11) {
 
 //Basic Exercise 2
 //console.log(Math.max(1, 2, 3));
-
+/*
 var array1 = [10000, 2, 3, 234];
 console.log(Math.max(...array1));
+*/
+
+//Basic Exercise 3
+/*
+var temperature = (Math.random() * 40) - 5
+if (temperature > 20) {
+    document.write("The weather is hot")
+
+} else if (temperature > 10) {
+    document.write("The weather is moderate")
+
+} else {
+    document.write("The weather is cold")
+
+}
+*/
