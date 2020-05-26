8a7975d9da2335e3cee3083ab591421d42c91396
Beniamin Hutyra <hutyra.beniamin@outlook.com>
2020-05-26 10:55:36 +0200

    add content

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-var randomTemp = Math.floor(Math.random() * 31) - 5;
+//Basic Exercise 1
+/*var randomTemp = Math.floor(Math.random() * 31) - 5;
 document.write(randomTemp);
 
 if (randomTemp > 11) {
     document.write(" The weather is moderate ");
 } else {
     document.write(" The weather is cold ");
-}
+}*/
+
+
+//Basic Exercise 2
+//console.log(Math.max(1, 2, 3));
+
+var array1 = [10000, 2, 3, 234];
+console.log(Math.max(...array1));
