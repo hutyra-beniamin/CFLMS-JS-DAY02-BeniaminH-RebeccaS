b2e88bb2775a092f445a8592f117c5b6f44c7863
Rebecca Schedler <rebecca.schedler@gmx.net>
2020-05-26 10:30:12 +0200

    exercise 1

---
+++ script.js
@@ -0,0 +1,8 @@
+var randomTemp = Math.floor(Math.random() * 31) -5;
+document.write(randomTemp);
+
+if (randomTemp > 100) {
+   document.write("a is greater than 100")
+} else {
+   document.write("a is less than or equal to 100")
+}
